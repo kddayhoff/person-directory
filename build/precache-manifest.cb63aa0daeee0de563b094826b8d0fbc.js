4270773de0cd1f643307e59f8057c96eb42a28b1
Karla Dayhoff <kddayhoff@gmail.com>
2020-06-25 16:19:39 -0500

    removed React favicon

---
+++ build/precache-manifest.cb63aa0daeee0de563b094826b8d0fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f43483fa2da2c99fac0461ec7cf7825",
+    "url": "/person-directory/index.html"
+  },
+  {
+    "revision": "596a3c0f3e0142f625b1",
+    "url": "/person-directory/static/js/2.3ce282ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/person-directory/static/js/2.3ce282ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51f4a18f6f037b68c97",
+    "url": "/person-directory/static/js/main.4ffdc7d5.chunk.js"
+  },
+  {
+    "revision": "abcd5be36581bb372ea5",
+    "url": "/person-directory/static/js/runtime-main.3506cb82.js"
+  }
+]);
